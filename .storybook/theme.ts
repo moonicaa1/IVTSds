bf2aee89668dbe8b1d1bd16ea753868c193e229b
김승연 (Seungyeon Kim) <sykim@inventis.co.kr>
2026-02-03 18:35:28 +0900

    feat: setup cloudflare pages deployment and custom storybook theme

---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'light',
+    brandTitle: 'Inventis',
+    brandUrl: 'https://www.inventis.co.kr',
+    brandImage: 'https://intra.inventis.co.kr/theme/inventis/img/logo-color.png',
+    brandTarget: '_self',
+});
