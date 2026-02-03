fb17ddfe3cb77167d2d9e770bc5c8cf429baf801
김승연 (Seungyeon Kim) <sykim@inventis.co.kr>
2026-02-03 10:43:10 +0900

    style: enhance storybook theme and preview layouts

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -5,22 +5,34 @@ import { ThemeProvider } from "../lib/contexts/ThemeContext";
 
 const preview: Preview = {
   parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
-    // layout: "fullscreen" 을 제거하고 기본값(centered 등)을 따르거나 각 스토리에서 결정하도록 합니다.
-    a11y: {
-      test: "todo",
+    docs: {
+      toc: true, // 목차 추가
     },
   },
   decorators: [
     (Story) => (
       <ThemeProvider>
-        <div className="bg-backgroundSecondary" style={{ minHeight: '100%', padding: '1rem' }}>
-          <Story />
+        <div
+          className="bg-backgroundSecondary text-contentPrimary font-sans"
+          style={{
+            minHeight: '100%',
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: '40px'
+          }}
+        >
+          <div style={{ width: '100%', maxWidth: '1200px' }}>
+            <Story />
+          </div>
         </div>
       </ThemeProvider>
     ),
